404c67c4917a1b51bdffc35cf3e3fe20b3f5574d
Ben Delaney <bendelaney@gmail.com>
2025-08-29 17:42:15 -0700

    favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
   description: "The fastest way to view your Jobber Visits.",
   icons: {
     icon: [
+      { url: '/favicon.ico' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
